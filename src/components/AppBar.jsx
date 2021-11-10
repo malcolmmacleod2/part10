c5a5c366f62fe937c337fe8709abe56d22a4f7e5
Malcolm Macleod <malcolm.macleod@macleodhome.com>
2021-11-10 21:31:58 +0000

    10.4 Add app bar

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet, Text, Pressable } from 'react-native';
+import Constants from 'expo-constants';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight * 2,
+    backgroundColor: theme.colors.headingBackground,
+    paddingBottom: Constants.statusBarHeight,
+    paddingLeft: 10
+    // ...
+  },
+  text: {
+      color: theme.colors.headingColor,
+      fontSize: 24,
+      fontWeight: 'bold'
+  }
+});
+
+const AppBar = () => {
+  return <View style={styles.container}><Pressable><Text style={styles.text}>Repositories</Text></Pressable></View>;
+};
+
+export default AppBar;
