8953202b13f741cbe341bae93a73ce982a158a4a
Malcolm Macleod <malcolm.macleod@macleodhome.com>
2022-01-15 20:31:31 +0000

    10.20 - repository review list (could not get wrapping to work)

--- src/components/SingleRepository.jsx
+++ src/components/SingleRepository.jsx
@@ -1,8 +1,99 @@
 import React from 'react';
-import { View, Button, Linking } from 'react-native';
+import {
+  View,
+  Button,
+  Linking,
+  FlatList,
+  Text,
+  StyleSheet,
+} from 'react-native';
 import { useQuery } from '@apollo/client';
 import { GET_REPOSITORY } from '../graphql/queries';
 import RepositoryItem from './RepositoryItem';
+import { format } from 'date-fns';
+import { parseISO } from 'date-fns/esm';
+import theme from '../theme';
+
+const singleRepositoryStyles = StyleSheet.create({
+  circle: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    borderColor: theme.colors.textSecondary,
+    borderWidth: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+});
+
+const reviewStyles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flexGrow: 1,
+    padding: 5,
+  },
+  infoContainer: {
+    flexDirection: 'column',
+    margin: 20,
+  },
+  heading: {
+    fontSize: theme.fontSizes.heading,
+    fontWeight: 'bold',
+  },
+  subheading: {
+    fontSize: theme.fontSizes.subheading,
+    color: theme.colors.textSecondary,
+    fontWeight: 'bold',
+  },
+  separator: {
+    height: 5,
+    width: '100%',
+    backgroundColor: '#000',
+  },
+  textRow: {
+    flex: 1,
+  },
+  textWrap: {
+    flex: 1,
+    flexWrap: 'wrap',
+  },
+});
+
+const RepositoryInfo = ({ repository }) => {
+  return (
+    <View testID="singleRepositoryView">
+      <RepositoryItem item={repository}></RepositoryItem>
+      <Button
+        title="Open in GitHub"
+        onPress={() => Linking.openURL(repository.url)}
+      ></Button>
+    </View>
+  );
+};
+
+const ReviewItem = ({ review }) => {
+  const createdDate = format(parseISO(review.createdAt), 'dd.MM.yyyy');
+  // Single review item
+  return (
+    <View style={reviewStyles.container}>
+      <View style={singleRepositoryStyles.circle}>
+        <Text>{review.rating}</Text>
+      </View>
+      <View style={reviewStyles.infoContainer}>
+        <Text style={reviewStyles.heading}>{review.user.username}</Text>
+        <Text style={reviewStyles.subheading}>{createdDate}</Text>
+        <View style={reviewStyles.textRow}>
+          <Text>{review.text}</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const RepositorySeparator = () => {
+  return <View style={reviewStyles.separator}></View>;
+};
 
 const SingleRepository = ({ repoId }) => {
   const { loading, error, data } = useQuery(GET_REPOSITORY, {
@@ -15,14 +106,21 @@ const SingleRepository = ({ repoId }) => {
 
   const repository = data?.repository;
 
+  const reviews = repository.reviews
+    ? repository.reviews.edges.map((edge) => edge.node)
+    : [];
+
+  console.log({ reviews });
+
   return (
-    <View testID="singleRepositoryView">
-      <RepositoryItem item={repository}></RepositoryItem>
-      <Button
-        title="Open in GitHub"
-        onPress={() => Linking.openURL(repository.url)}
-      ></Button>
-    </View>
+    <FlatList
+      data={reviews}
+      renderItem={({ item }) => <ReviewItem review={item} />}
+      keyExtractor={({ id }) => id}
+      ListHeaderComponent={() => <RepositoryInfo repository={repository} />}
+      ItemSeparatorComponent={() => <RepositorySeparator></RepositorySeparator>}
+      // ...
+    />
   );
 };
 
